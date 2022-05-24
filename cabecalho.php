3607f495ff35c391d4d9ef978d2e89bc5c604d82
Matheus Felipe Brandão da Silva <77033014+MatheusBrandao525@users.noreply.github.com>
2022-05-24 16:47:51 -0300

    criação dos diretorios e arquivos iniciais

---
+++ cabecalho.php
@@ -0,0 +1,35 @@
+<div id="cabecalho-superior">
+    <nav id="menu-cima">
+        <ul>
+            <li><a href="#">Minha conta</a></li>
+            <li><a href="#">Meu carrinho</a></li>
+            <li><a href="#">Logar</a></li>
+            <li><a href="#">Cadastrar</a></li>
+        </ul>
+    </nav>
+</div>
+
+<div id="cabecalho-meio" class="fundo-azul">
+    <h1>mBrandao - os melhores cursos estão aqui</h1>
+    <p>Nenhum item no seu carrinho de compras</p>
+    <section>
+        <form action="" method="post">
+            <label for="">
+                <span>Buscar</span>
+                <input type="search" name="pesquisa" id="pesquisa">
+                <input type="image" src="imagens/lupa.png">
+            </label>
+        </form>
+    </section>
+</div>
+
+<div id="cabecalho-inferio">
+    <nav id="menu-principal">
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Produtos</a></li>
+            <li><a href="#">Fale conosco</a></li>
+            <li><a href="#">Quem somos</a></li>
+        </ul>
+    </nav>
+</div>
---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>MBrandao - Cursos online</title>
+</head>
+<body>
+    <main id="principal">
+        <section id="cabecalho">
+            <?php include_once("cabecalho.php");?>
+        </section><!--Fin cabecalho-->
+
+        <section id="corpo">
+
+        </section><!--FIm corpo-->
+
+        <footer id="rodape">
+
+        </footer><!--Fim rodapé-->
+    </main>
+</body>
+</html>
